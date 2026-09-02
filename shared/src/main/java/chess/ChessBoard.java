4084075711275ba319aa45c92c020a19b6f17162
Dylan Waite <dylanwaite03@gmail.com>
2026-09-02 14:02:05 -0600

    made reset board function

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -46,7 +46,44 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        board[0][0]= new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.ROOK);
+        board[0][1]= new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.KNIGHT);
+        board[0][2]= new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.BISHOP);
+        board[0][3]= new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.QUEEN);
+        board[0][4]= new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.KING);
+        board[0][5]= new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.BISHOP);
+        board[0][6]= new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.KNIGHT);
+        board[0][7]= new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.ROOK);
+
+        board[1][0]=new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN);
+        board[1][1]=new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN);
+        board[1][2]=new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN);
+        board[1][3]=new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN);
+        board[1][4]=new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN);
+        board[1][5]=new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN);
+        board[1][6]=new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN);
+        board[1][7]=new ChessPiece(ChessGame.TeamColor.WHITE,ChessPiece.PieceType.PAWN);
+
+        board[7][0]= new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.ROOK);
+        board[7][1]= new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.KNIGHT);
+        board[7][2]= new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.BISHOP);
+        board[7][3]= new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.QUEEN);
+        board[7][4]= new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.KING);
+        board[7][5]= new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.BISHOP);
+        board[7][6]= new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.KNIGHT);
+        board[7][7]= new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.ROOK);
+
+        board[6][0]=new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN);
+        board[6][1]=new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN);
+        board[6][2]=new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN);
+        board[6][3]=new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN);
+        board[6][4]=new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN);
+        board[6][5]=new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN);
+        board[6][6]=new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN);
+        board[6][7]=new ChessPiece(ChessGame.TeamColor.BLACK,ChessPiece.PieceType.PAWN);
+
+
+
     }
 
 
