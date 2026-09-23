607dc06188410d124f2aa7bb2c784da7d7979539
Dylan Waite <dylanwaite03@gmail.com>
2026-09-23 14:17:22 -0600

    did the starter code stuff and made all the folders and so that I am ready to start working on part 3 of the project.

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -4,7 +4,10 @@ import chess.*;
 
 public class ServerMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        var port = 8080;
+        Server server = new Server();
+        server.run(port);
+
+        System.out.println("♕ 240 Chess Server running on port " + port);
     }
 }
