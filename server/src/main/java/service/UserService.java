5ef318ac14a48382c7e87b322430e7817e17d06d
Dylan Waite <dylanwaite03@gmail.com>
2026-09-26 08:49:12 -0600

    added the register function to UserService.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,6 +1,7 @@
 package service;
 
 import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
 import dataaccess.UserDAO;
 import model.AuthData;
 import model.UserData;
@@ -13,6 +14,22 @@ public class UserService {
     public UserService(UserDAO userDAO, AuthDAO authDAO){
         this.userDAO=userDAO;
         this.authDAO=authDAO;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws Exception{
+        UserData existingUser=userDAO.getUser(request.username());
+
+        if (existingUser!=null){
+            throw new Exception("already taken");
+
+        }
+
+        UserData user=new UserData(request.username(),request.password(),request.email());
+        userDAO.createUser(user);
+        String authToken=UUID.randomUUID().toString();
+        AuthData authData= new AuthData(authToken, request.username());
+        authDAO.createAuth(authData);
+        return new RegisterResult(request.username(), authToken);
     }
 
 }
