e0f2edd25f881dcaf7c65b54a4a8908af33877f2
Dylan Waite <dylanwaite03@gmail.com>
2026-09-26 08:12:39 -0600

    added authdao interface, now im starting to make the class with the actual functions

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,10 @@
+package dataaccess;
+
+import model.AuthData;
+
+public interface AuthDAO {
+    void createAuth(AuthData authdata) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(AuthData authData) throws DataAccessException;
+    void clear() throws DataAccessException;
+}
