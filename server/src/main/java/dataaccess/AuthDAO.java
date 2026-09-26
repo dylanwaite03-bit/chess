89c8f31a7a832b953165e2179c94651227f18273
Dylan Waite <dylanwaite03@gmail.com>
2026-09-26 08:23:53 -0600

    finished making the simple functions inside MemoryauthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -5,6 +5,6 @@ import model.AuthData;
 public interface AuthDAO {
     void createAuth(AuthData authdata) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
-    void deleteAuth(AuthData authData) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
     void clear() throws DataAccessException;
 }
--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -1,4 +1,33 @@
 package dataaccess;
 
-public class MemoryAuthDAO {
+import model.AuthData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MemoryAuthDAO implements AuthDAO{
+    private final Map<String, AuthData> authTokens=new HashMap<>();
+
+    @Override
+    public void createAuth(AuthData authdata) throws DataAccessException{
+        authTokens.put(authdata.authtoken(), authdata);
+    }
+
+    @Override
+    public AuthData getAuth(String authtoken) throws DataAccessException{
+        return authTokens.get(authtoken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authTokens.remove(authToken);
+    }
+
+    @Override
+    public void clear() throws DataAccessException{
+        authTokens.clear();
+    }
+
+
 }
